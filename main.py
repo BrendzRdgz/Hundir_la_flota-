900564d9b9e94fc7133f56bf89719f2abe1d834f
franpujalte <franpujalte1@gmail.com>
2024-03-01 21:28:21 +0100

    hola

---
+++ main.py
@@ -0,0 +1,15 @@
+import variables
+import FUNCIONES_2
+print(tablero_1)
+'''
+if __name__ == "__main__":
+    print('Máquina, te dejo que empieces primero, un poco de ventaja no te vendrá mal...')
+    juego_activo = True
+    while juego_activo == True:
+    #input para introducir las coords de disparo
+        disparar(tablero_1.tablero_maquina)
+        comprobar_victoria(tablero_1.tablero_maquina)
+
+        disparar_maquina(tablero_1.tablero_humano)
+        comprobar_victoria_maquina(tablero_1.tablero_humano)
+'''
---
+++ variables.py
@@ -0,0 +1,6 @@
+from tablero import*
+
+tablero_1 = Tablero()
+
+abecedario = 'abcdefghijklmnopqrstuvwxyz'
+orientaciones = ["N","S","O","E"]
